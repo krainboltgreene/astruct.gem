15754bd1bf6059ab83ace9762ec131b921101fa5
Kurtis Rainbolt-Greene <kurtisrainboltgreene@gmail.com>
2012-08-24 12:57:42 -0500

    Adding a benchmark for the delete and delete field methods

---
+++ benchmark/delete_vs_ostruct.rb
@@ -0,0 +1,19 @@
+require 'benchmark/ips'
+require_relative '../lib/astruct'
+require 'ostruct'
+
+DATA = (1..1_000).map { |i| { :"item#{i}" => i } }.inject :merge!
+
+Benchmark.ips do |x|
+  x.report "OStruct load" do
+    class OProfile < OpenStruct; end
+    op = OProfile.new DATA.dup
+    op.delete_field :item1
+  end
+
+  x.report "AStruct load" do
+    class AProfile < AltStruct; end
+    ap = AProfile.new DATA.dup
+    ap.delete :item1
+  end
+end
