f25a98d7da6cec2f8133681383b2415df0782551
Kurtis Rainbolt-Greene <kurtisrainboltgreene@gmail.com>
2012-08-23 22:20:51 -0500

    Breaking up and refactoring tests

--- test/lib/astruct_test.rb
+++ test/lib/astruct_test.rb
@@ -1,9 +1,5 @@
 require 'minitest/autorun'
-require '../helper'
-
-class Profile
-  include AltStruct::M
-end
+require_relative '../helper'
 
 class TestAltStruct < MiniTest::Unit::TestCase
   def setup
@@ -11,62 +7,67 @@ class TestAltStruct < MiniTest::Unit::TestCase
     @example = AltStruct.new name: "Kurtis", age: 24
   end
 
-  def test_equality
-    struct2 = AltStruct.new
-    assert_equal(@empty, struct2)
+  def test_equality_with_two_empty_astructs
+    empty2 = AltStruct.new
+    assert_equal @empty, empty2
+  end
 
-    @empty.a = 'a'
-    refute_equal(@empty, struct2)
+  def test_equality_vs_astruct_with_fields
+    refute_equal @example, @empty
+  end
 
-    struct2.a = 'a'
-    assert_equal(@empty, struct2)
-
-    @empty.a = 'b`'
-    refute_equal(@empty, struct2)
-
-    struct3 = Object.new
-    struct3.instance_eval { @table = {:a => 'b'} }
-    refute_equal(@empty, struct3)
+  def test_equality_vs_object_with_fake_table
+    empty_object = Object.new
+    empty_object.instance_eval { @table = { name: "Kurtis", age: 24 }  }
+    expected = @example
+    actual = empty_object
+    refute_equal expected, actual
   end
 
   def test_inspect_with_no_fields
-    assert_equal("#<AltStruct>", @empty.inspect)
+    expected = "#<AltStruct>"
+    actual = @empty.inspect
+    assert_equal expected, actual
   end
 
   def test_inspect_with_fields
     @empty.example1 = 1
     @empty.example2 = 2
-    assert_equal("#<AltStruct example1=1, example2=2>", @empty.inspect)
+    expected = "#<AltStruct example1=1, example2=2>"
+    actual = @empty.inspect
+    assert_equal expected, actual
   end
 
   def test_inspect_with_sub_struct
     @empty.struct2 = AltStruct.new
     @empty.struct2.struct3 = @empty
-    assert_equal('#<AltStruct struct2=#<AltStruct struct3=#<AltStruct ...>>>', @empty.inspect)
+    expected = '#<AltStruct struct2=#<AltStruct struct3=#<AltStruct ...>>>'
+    actual = @empty.inspect
+    assert_equal expected, actual
   end
 
   def test_inspect_with_empty_sub_struct
     @empty.struct2 = AltStruct.new
-    assert_equal('#<AltStruct struct2=#<AltStruct>>', @empty.inspect)
+    expected = '#<AltStruct struct2=#<AltStruct>>'
+    actual = @empty.inspect
+    assert_equal expected, actual
   end
 
   def test_freeze_stops_new_assignments
     @example.freeze
     assert_raises(RuntimeError) { @example.age = 24 }
-    # assert_raises(TypeError) { @example.age = 24 }
   end
 
   def test_freeze_still_returns_values
     @example.freeze
-    expects = "Kurtis"
+    expecteds = "Kurtis"
     actual =  @example.name
-    assert_equal expects, actual
+    assert_equal expecteds, actual
   end
 
   def test_freeze_stops_reassignments
     @example.freeze
     assert_raises(RuntimeError) { @example.name = "Jazzy" }
-    # assert_raises(TypeError) { @example.name = "Jazzy" }
   end
 
   # def test_freeze_stops_reassign_even_if_frozen_redefined
@@ -78,26 +79,41 @@ class TestAltStruct < MiniTest::Unit::TestCase
   #   # assert_raises(TypeError, message) { @example.name = "Jazzy" }
   # end
 
-  def test_delete_field
+  def test_astruct_doesn_respond_to_non_existant_keys_getter
+    refute_respond_to @empty, :akey
+  end
+
+  def test_astruct_doesn_respond_to_non_existant_keys_setter
+    refute_respond_to @empty, :akey=
+  end
+
+  def test_delete_field_removes_getter_method
     bug = '[ruby-core:33010]'
+    @example.delete_field :name
+    refute_respond_to @example, :name, bug
+  end
 
-    refute_respond_to(@empty, :akey)
-    refute_respond_to(@empty, :akey=)
+  def test_delete_field_removes_setter_method
+    bug = '[ruby-core:33010]'
+    @example.delete_field :name
+    refute_respond_to @example, :name=, bug
+  end
 
-    @empty.akey = 'avalue'
-    assert_respond_to(@empty, :akey)
-    assert_respond_to(@empty, :akey=)
+  def test_delete_field_removes_table_key_value
+    @example.delete_field :name
+    expected = nil
+    actual = @example.table[:name]
+    assert_equal expected, actual
+  end
 
-    avalue = @empty.delete_field :akey
-    refute_respond_to(@empty, :akey, bug)
-    refute_respond_to(@empty, :akey=, bug)
-
-    assert_equal(avalue, 'avalue')
+  def test_delete_field_returns_value_of_deleted_key
+    expected = "Kurtis"
+    actual = @example.delete_field :name
+    assert_equal expected, actual
   end
 
   def test_method_missing_handles_square_bracket_equals
     assert_raises(ArgumentError) { @empty[:foo] = :bar }
-    # assert_raises(NoMethodError) { @empty[:foo] = :bar }
   end
 
   def test_method_missing_handles_square_brackets
@@ -105,16 +121,16 @@ class TestAltStruct < MiniTest::Unit::TestCase
   end
 
   def test_to_hash_returns_hash
-    expect = { name: "John Smith", age: 70, pension: 300 }
-    actual = AltStruct.new(expect).to_hash
-    assert_equal expect, actual
+    expected = { name: "John Smith", age: 70, pension: 300 }
+    actual = AltStruct.new(expected).to_hash
+    assert_equal expected, actual
   end
 
   def test_to_hash_modified_modifies_astruct
     @example.to_hash[:age] = 70
-    expect = 70
+    expected = 70
     actual = @example.age
-    assert_equal expect, actual
+    assert_equal expected, actual
   end
 
   def test_that_example_has_table_method
@@ -122,9 +138,9 @@ class TestAltStruct < MiniTest::Unit::TestCase
   end
 
   def test_that_empty_example_has_empty_table
-    expect = {}
+    expected = {}
     actual = @empty.table
-    assert_equal expect, actual
+    assert_equal expected, actual
   end
 
   def test_that_example_has_name_method
@@ -132,9 +148,9 @@ class TestAltStruct < MiniTest::Unit::TestCase
   end
 
   def test_that_example_has_given_name
-    expect = "Kurtis"
+    expected = "Kurtis"
     actual = @example.name
-    assert_equal expect, actual
+    assert_equal expected, actual
   end
 
   def test_that_example_takes_name
@@ -143,9 +159,9 @@ class TestAltStruct < MiniTest::Unit::TestCase
 
   def test_that_example_has_taken_name
     @example.name = "Dave"
-    expect = "Dave"
+    expected = "Dave"
     actual = @example.name
-    assert_equal expect, actual
+    assert_equal expected, actual
   end
 
   def test_that_load_takes_a_hash
@@ -159,9 +175,9 @@ class TestAltStruct < MiniTest::Unit::TestCase
 
   def test_that_load_sets_correct_value
     @example.load nickname: "Kurt"
-    expect = "Kurt"
+    expected = "Kurt"
     actual = @example.nickname
-    assert_equal expect, actual
+    assert_equal expected, actual
   end
 
   def test_that_example_has_dump_method
@@ -169,21 +185,21 @@ class TestAltStruct < MiniTest::Unit::TestCase
   end
 
   def test_that_dump_contains_values
-    expect = { name: "Kurtis", age: 24 }
+    expected = { name: "Kurtis", age: 24 }
     actual = @example.dump
-    assert_equal expect, actual
+    assert_equal expected, actual
   end
 
   def test_that_selective_dump_contains_selective_values
-    expect = { age: 24 }
+    expected = { age: 24 }
     actual = @example.dump :age
-    assert_equal expect, actual
+    assert_equal expected, actual
   end
 
   def test_inspect_has_values
-    expect = /name="Kurtis", age=24/
+    expected = /name="Kurtis", age=24/
     actual = @example.inspect
-    assert_match expect, actual
+    assert_match expected, actual
   end
 
   def test_that_other_class_isnt_affected
