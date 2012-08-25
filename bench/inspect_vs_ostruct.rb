0542920161cbb64b9c97f6105b0a7ddd65e5bd92
Kurtis Rainbolt-Greene <kurtisrainboltgreene@gmail.com>
2012-08-25 03:15:42 -0500

    Using larger datasets

--- bench/inspect_vs_ostruct.rb
+++ bench/inspect_vs_ostruct.rb
@@ -2,7 +2,7 @@ require 'benchmark/ips'
 require 'astruct'
 require 'ostruct'
 
-DATA = (1..1_000).map { |i| { :"item#{i}" => i } }.inject :merge!
+DATA = (1..10_000).map { |i| { :"item#{i}" => i } }.inject :merge!
 
 Benchmark.ips do |x|
   x.report "OStruct load" do
